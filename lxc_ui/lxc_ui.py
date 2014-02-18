65ce3117e93f386a7b7332ed7f0efeee16075a5d
rasmuskr <github@rasmuskr.dk>
2014-02-18 08:11:13 +0000

    adding bootstrap and jquery to begin doing some UI wok

--- lxc_ui/lxc_ui.py
+++ lxc_ui/lxc_ui.py
@@ -1,1 +1,27 @@
+import logging
 
+logger = logging.getLogger(__name__)
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def root():
+    headers = {
+        "Location": "/static/index.html",
+    }
+    return flask.Response("Redirecting", status=302, headers=headers)
+
+
+if __name__ == "__main__":
+    logging.root.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logging.root.addHandler(handler)
+
+    # load config
+    app.config.from_pyfile('application.cfg', silent=True)
+    app.run("0.0.0.0", 5000, debug=True)
